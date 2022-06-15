2339350fe605dc7a2a685ad58a1611d5e48e5fef
l bates childs <labchilds@gmail.com>
2022-06-15 16:15:38 -0400

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import pdf from "../../assets/documents/lbchilds-resume.pdf";
+import pdf from "../../assets/documents/resume.pdf";
 
 const Resume = () => {
   return (
