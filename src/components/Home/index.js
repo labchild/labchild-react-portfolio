8c00609e3a8037208701af76c0677667e9cc7fad
l bates childs <labchilds@gmail.com>
2022-07-01 16:35:46 -0400

    play around with marquees

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,12 +6,22 @@ const Home = () => {
   // const { handleSectionSelect } = props;
 
   return (
-    <div className="bg-animation">
-      <div className="wrap">
+    <>
+      {/* <div className="wrap">
         <div className="top-plane"></div>
         <div className="bottom-plane"></div>
-      </div>
-      <h1 className="title">labchild</h1>
+      </div> */}
+      <h1 className="marquee">
+        {/* <div className="marquee-row"> */}
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+          <div className="title marquee-item">labchild</div>
+        {/* </div> */}
+      </h1>
       <h2 class="subtitle">Lelah Bates Childs</h2>
       <p>welcome to my website, where you can learn more about me</p>
       {/* <div className="section-list">
@@ -35,24 +45,14 @@ const Home = () => {
           menus &#38; fashion &#8226; net art multi-media &#38; paint
         </p>
       </div> */}
-      <div className="marquee">
-        <p className="marquee-1">
-          html &#8226; css &#8226; javascript &#8226;{" "}
-        </p>
-        <p className="marquee-2">
-          ui/ux &#8226; server &#8226; database &#8226;{" "}
-        </p>
-        <p className="marquee-3">
-          photoshop &#8226; illustrator &#8226; indesign &#8226;{" "}
-        </p>
-        <p className="marquee-4">
-          net art &#8226; multi-media &#8226; paint &#8226;{" "}
-        </p>
-        <p className="marquee-5">
-          graphic design &#8226; fashion design &#8226; cocktail design &#8226;{" "}
-        </p>
+      <div>
+        <p>html &#8226; css &#8226; javascript</p>
+        <p>ui/ux &#8226; server &#8226; database</p>
+        <p>photoshop &#8226; illustrator &#8226; indesign</p>
+        <p>net art &#8226; multi-media &#8226; paint</p>
+        <p>graphic design &#8226; fashion design &#8226; cocktail design</p>
       </div>
-    </div>
+    </>
   );
 };
 
