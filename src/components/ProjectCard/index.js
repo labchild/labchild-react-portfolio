936a45361290df43092f9e448ce829ada170ffab
l bates childs <labchilds@gmail.com>
2022-08-07 03:15:35 -0400

    json file for work

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import projects from "../../assets/documents/work.json";
+
 import "./index.css";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+  const { name, hosted, url, img_path, img_alt, github, description } = props;
+//   const imgSrc = require(`../../assets/images/${img_path}`);
   return (
     <article className="project-card card">
-      <div className="card-img">image</div>
+      <div className="card-img">
+        {/* <img src={imgSrc} alt={img_alt} /> */}
+      </div>
       <div className="card-text">
-        <div className="card-head">title</div>
-        <div className="card-body">stuff</div>
+        {hosted === "false" ? (
+          <div className="card-head">{name}</div>
+        ) : (
+          <div className="card-head">
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {name}
+            </a>
+          </div>
+        )}
+        <div className="card-body">
+            <p>{description}</p>
+            <a href={github} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon="fa-brands fa-github" /> {name} on Github
+            </a>
+        </div>
       </div>
     </article>
   );
--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ProjectCard from "../ProjectCard";
 import projects from "../../assets/documents/work.json";
 
+// const previewImgs = projects.map(project => require(`../../assets/images/previews/${project.img_path}`) );
+
 const ProjectList = () => {
   // pass image, project name, deployed url and gh repo url as props
   // .map the array of data
@@ -9,7 +11,19 @@ const ProjectList = () => {
     <section className="content">
       <h1 className="title">projects</h1>
       <div className="details">
-        {projects.length > 0 ? ( projects.map(project => <ProjectCard name={project} key={projects.indexOf(project)} />)
+        {projects.length > 0 ? (
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              hosted={project.hosted}
+              url={project.url}
+              img_path={project.img_path}
+              img_alt={project.img_alt}
+              github={project.github}
+              description={project.description}
+              key={projects.indexOf(project)}
+            />
+          ))
         ) : (
           <p>under construction until July 5 2022</p>
         )}
